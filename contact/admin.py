306f347fa0c6e9dd2d649e78c7617e13745afa1c
consenzzo <consenzzo@gmail.com>
2023-10-02 11:28:04 -0300

    criando urls para nomes

--- contact/admin.py
+++ contact/admin.py
@@ -3,13 +3,14 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id','first_name', 'last_name', 'phone',
+    list_display = 'id','first_name', 'last_name', 'phone', 'show',
     ordering = '-id', #Com o '-' ele coloca em ordem decrescente
     # list_filter = 'created_date',
     search_fields = 'id', 'first_name' , 'last_name',
     list_per_page = 20
     list_max_show_all = 50
     list_display_links = 'id', 'first_name',
+    list_editable = 'show',
 
 
 @admin.register(models.Category)
