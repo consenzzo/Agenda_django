ac830b0e77a6a24c7e934a652837ad6a6b6da442
consenzzo <consenzzo@gmail.com>
2023-10-02 07:52:29 -0300

    COnfigurando Contact/admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id','first_name', 'last_name', 'phone',
+    ordering = '-id', #Com o '-' ele coloca em ordem decrescente
+    # list_filter = 'created_date',
+    search_fields = 'id', 'first_name' , 'last_name',
+    list_per_page = 10
+    list_max_show_all = 50
+    list_display_links = 'id', 'first_name',
