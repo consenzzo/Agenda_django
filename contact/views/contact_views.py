c7db4002b48a561c18f3baa89f8610ba0bde6d90
consenzzo <consenzzo@gmail.com>
2023-10-02 12:06:41 -0300

    Usando a request no template para pegar o valor de GET

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import get_object_or_404, render
-
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 
@@ -10,6 +10,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -24,13 +25,45 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(
         request,
         'contact/contact.html',
+        context
+    )
+
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value) |
+            Q(last_name__icontains=search_value),
+            )\
+        
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
+        'search_value': search_value,
+    }
+
+    return render(
+        request,
+        'contact/index.html',
         context
     )
